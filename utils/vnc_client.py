a7f1e88c95d335ecdcd06415a98253aa48fb707c
Helloyunho <yunho050840@gmail.com>
2024-06-18 01:47:01 +0900

    ⚗️ fix: kay maybe im a bit stupid

--- utils/vnc_client.py
+++ utils/vnc_client.py
@@ -8,6 +8,19 @@ from PIL.Image import Image
 FPS = 60
 
 
+class CustomVNCClient(VNCDoToolClient):
+    on_ready: Callable[[], None] | None
+
+    def __init__(self, on_ready=None):
+        super().__init__()
+        self.on_ready = on_ready
+
+    async def vncConnectionMade(self):
+        await super().vncConnectionMade()
+        if self.on_ready:
+            self.on_ready()
+
+
 class VNCClient(threading.Thread):
     vnc: VNCDoToolClient
     on_close: Callable[[], None] | None
@@ -15,7 +28,7 @@ class VNCClient(threading.Thread):
 
     def __init__(self, on_close=None, on_screen_update=None):
         super().__init__()
-        self.vnc = VNCDoToolClient()
+        self.vnc = CustomVNCClient()
         self.on_close = on_close
         self.on_screen_update = on_screen_update
 
@@ -57,8 +70,11 @@ class VNCClient(threading.Thread):
         loop = asyncio.get_running_loop()
         loop.create_task(self.vnc.mousePress(button))
 
+    def on_ready(self):
+        loop = asyncio.get_running_loop()
+        loop.create_task(self.vnc_refresh_loop())
+
     async def vnc_refresh_loop(self):
-        await asyncio.sleep(1)
         while True:
             if self.vnc.writer.is_closing():
                 if self.on_close:
