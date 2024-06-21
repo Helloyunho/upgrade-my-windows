02864d4710c4b228a620bf13352e52652e870c3d
Helloyunho <yunho050840@gmail.com>
2024-06-22 03:16:44 +0900

    ✨ feat: imma try opencv

--- utils/display_window.py
+++ utils/display_window.py
@@ -1,14 +1,16 @@
+import cv2
 import pygame
 import threading
-from PIL import Image, ImageOps
+import queue
+import numpy as np
+from PIL import Image
 from utils.logger import get_logger
 
 
 class DisplayWindow(threading.Thread):
     def __init__(self):
         super().__init__(daemon=True)
-        pygame.mixer.pre_init(44100, -16, 2, buffer=512)
-        self.screen = None
+        self.screen = queue.Queue()
         self.running = True
         self.logger = get_logger(self.__class__.__name__)
 
@@ -16,30 +18,19 @@ class DisplayWindow(threading.Thread):
         self.running = False
 
     def update_frame(self, image: Image.Image):
-        if not self.screen:
-            return
-        self.screen.fill((0, 0, 0))
-        _image = ImageOps.contain(image, (1600, 900), Image.Resampling.LANCZOS)
-        mode = _image.mode
-        size = _image.size
-        data = _image.tobytes()
-
-        pygame_image = pygame.image.fromstring(data, size, mode)  # type: ignore
-        image_width, image_height = _image.size
-        # center the image
-        x = (1600 - image_width) // 2
-        y = (900 - image_height) // 2
-        self.screen.blit(pygame_image, (x, y))
+        numpy_image = np.array(image)
+        img = cv2.cvtColor(numpy_image, cv2.COLOR_RGB2BGR)
+        self.screen.put(img)
 
     def update_audio(self, data: bytes):
         pygame.mixer.Sound(buffer=data).play()
 
     def run(self):
-        pygame.init()
-        pygame.display.set_caption("Upgrade My Windows")
-        self.screen = pygame.display.set_mode((1600, 900))
+        pygame.mixer.init(44100, -16, 2, buffer=512)
+        cv2.namedWindow("Upgrade My Windows", cv2.WINDOW_AUTOSIZE)
         while self.running:
-            pygame.event.pump()
-            pygame.display.flip()
+            if not self.screen.empty():
+                image = self.screen.get()
+                cv2.imshow("Upgrade My Windows", image)
 
-        pygame.quit()
+        cv2.destroyAllWindows()
